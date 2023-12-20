e16139929cc0e18b32be0cc03180375556b78482
Tushar Koshti <tushar@zettabolt.com>
2023-12-20 12:14:20 +0530

    Added loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+function loading() {
+  return (
+    <div className="flex justify-center">
+      <img className="h-96" src="spinner.svg" alt="loading..." />
+    </div>
+  );
+}
+
+export default loading;
